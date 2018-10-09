8c3d28ec118dd0bacdcbda9f7df8180dfa83373f
zhenhua wei <twei@zhenhuas-MacBook-Pro.local>
2018-10-09 12:40:27 -0700

    add library to call product api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var scraper = require('product-scraper');
+
 const redis = require('redis');
 const bluebird = require("bluebird");
 
@@ -8,8 +10,8 @@ const bluebird = require("bluebird");
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-// connect to Redis
-const client = redis.createClient();
+//connect to Redis
+const client = redis.createClient({ "host": "127.0.0.1", "port": "6379" });
 client.on('connect', () => {
     console.log(`connected to redis`);
 });
@@ -17,19 +19,35 @@ client.on('error', err => {
     console.log(`Error: ${err}`);
 });
 
-app.post('/{productId}', function (req, res) {
+app.get('/check', function (req, res) {
+  //res.send('Hello World');
 
-  // assume the details is returned from the Amazon Product API call
-  res.data.productDetails = {'category': 'electric', 'rate': ''};
+  // check redis first to see if the product details is available
+  client.hgetall('B002QYW8LW', function(err, object) {
 
-  // save to redis
-  client.hmsetAsync(productId, res.data.productDetails)
-                    .catch(e => console.log(e));
+    console.log(object);
 
-  // display
-  return res.json(res.data.productDetails);
+    if (err) {
 
+      // call product api
+      scraper.init('https://www.amazon.com/gp/product/B002QYW8LW/', function(data){
+          console.log(data);
+
+          // save to redis
+          client.hmset({'B002QYW8LW', 'price', data.price, 'title', data.title}).catch(e => console.log(e));
+
+          // display
+          return res.json(data);
+
+      });
+
+    } else {
+      return res.json(object);
+    }
+
+  });
 });
+
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
 });
